12dcb20f1cd62a37943433cd549ff0f8886826ed
Dovydas Gailiūnas <108748876+dovydas372@users.noreply.github.com>
2025-03-26 22:01:10 +0200

    dom korteliu zaidimas

--- JavaScript/API_DOM/OraiUzduotis/Orai.js
+++ JavaScript/API_DOM/OraiUzduotis/Orai.js
@@ -1,4 +1,6 @@
-const oruPaieskosLaukas = document.querySelector("input");
+const APIKEY = "C7z0uZm0kry5t8Gk0GsNx1IwCZI1NQy7c7CV2zgjkt2hIhxCUDNTpaPR";
+const oruPaieskosLaukas = document.querySelector("form");
+let miestoPavadinimas = oruPaieskosLaukas.elements["miestas"].value;
 
 const gautiIrSuvestiOraiData = async (paieskosLaukoData) => {
   try {
@@ -21,13 +23,28 @@ const gautiIrSuvestiOraiData = async (paieskosLaukoData) => {
     miestoLaukas.innerText = miestas;
     temperaturosLaukas.innerText = `${Math.round(temperatura)}  °C`;
     oroLaukas.innerText = oras;
-  } catch (error) {
-    alert("Blogai ivestas miesto pavadinimas");
-  }
-};
-// oruPaieskosLaukas.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   renderPosts2(searchForm.term.value.trim());
-// });
 
-gautiIrSuvestiOraiData();
+    pakeistiPaveiksleli(miestas);
+  } catch (error) {}
+};
+
+const pakeistiPaveiksleli = async (miestas) => {
+  const paveikslelis = await axios.get(
+    `https://api.pexels.com/v1/search?query=${miestas}`,
+    {
+      headers: {
+        Authorization: APIKEY,
+      },
+    }
+  );
+  const miestoPaveiklsoSRC = paveikslelis.data.photos[0].src.original;
+  document.body.style.backgroundImage = `url(${miestoPaveiklsoSRC})`;
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+};
+
+oruPaieskosLaukas.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let miestoPavadinimas = oruPaieskosLaukas.elements["miestas"].value;
+  gautiIrSuvestiOraiData(miestoPavadinimas);
+});
