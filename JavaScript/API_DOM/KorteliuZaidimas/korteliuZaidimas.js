385017b7fef47a9bb15387042d77c29730c245ca
Dovydas Gailiūnas <108748876+dovydas372@users.noreply.github.com>
2025-03-30 18:44:01 +0300

    portfolijo nepabaigtas

--- JavaScript/API_DOM/KorteliuZaidimas/korteliuZaidimas.js
+++ JavaScript/API_DOM/KorteliuZaidimas/korteliuZaidimas.js
@@ -1,6 +1,10 @@
-korteles.sort(() => 0.5 - Math.random());
-
+// korteles.sort(() => 0.5 - Math.random());
+let i = 0;
+let tikrinimoMasyvas = [];
+let paspaudimoMasyvas = [];
 const APIKEY = "C7z0uZm0kry5t8Gk0GsNx1IwCZI1NQy7c7CV2zgjkt2hIhxCUDNTpaPR";
+
+sumaisytiKortas();
 
 const pakeistiPaveiksleli = async (kortelesPav) => {
   const paveikslelis = await axios.get(
@@ -13,13 +17,59 @@ const pakeistiPaveiksleli = async (kortelesPav) => {
   );
   const miestoPaveiklsoSRC = paveikslelis.data.photos[0].src.medium;
   console.log(miestoPaveiklsoSRC);
+  return miestoPaveiklsoSRC;
 };
 
 const korteliuMasyvas = document.querySelectorAll(".korteliuLaukas > div");
 
 korteliuMasyvas.forEach((kortele, indeksas) => {
-  console.log("object");
   kortele.addEventListener("click", () => {
-    console.log(indeksas);
+    atverstiKortele(kortele);
+    console.log(kortele);
   });
 });
+
+function atverstiKortele(kortele) {
+  kortele.querySelector("img").style.display = "";
+  tikrinimoMasyvas.push(kortele.querySelector("img").src);
+  paspaudimoMasyvas.push(kortele);
+  if (tikrinimoMasyvas.length === 3) {
+    tikrinimoMasyvas = [];
+    tikrinimoMasyvas.push(kortele.querySelector("img").src);
+    paspaudimoMasyvas = [];
+    paspaudimoMasyvas.push(kortele);
+  }
+  if (tikrinimoMasyvas[0] === tikrinimoMasyvas[1]) {
+    pridetiRezultata();
+    console.log("jūs gavote tašką");
+    paspaudimoMasyvas.forEach((divas) => {
+      setTimeout(() => {
+        divas.style.display = "none";
+      }, 500);
+    });
+  } else {
+    console.log("Neatspėjote");
+  }
+  console.log(tikrinimoMasyvas);
+  setTimeout(() => {
+    kortele.querySelector("img").style.display = "none";
+  }, 2000);
+}
+
+pakeistiPaveiksleli("eifel");
+
+function pridetiRezultata() {
+  i++;
+  let rezultatas = document.getElementById("rezultatas");
+  rezultatas.innerHTML = i;
+}
+
+function sumaisytiKortas() {
+  let imgElements = document.querySelectorAll("img");
+  let srcMasyvas = Array.from(imgElements, (img) => img.src);
+
+  srcMasyvas.sort(() => 0.5 - Math.random());
+  imgElements.forEach((img, index) => {
+    img.src = srcMasyvas[index];
+  });
+}
