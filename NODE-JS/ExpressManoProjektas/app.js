2a541ad85336fbca5a5bf6de54bc567adbbef473
Dovydas Gailiūnas <108748876+dovydas372@users.noreply.github.com>
2026-05-03 23:04:45 +0300

    expressManoProjektas pakeitimai

--- NODE-JS/ExpressManoProjektas/app.js
+++ NODE-JS/ExpressManoProjektas/app.js
@@ -1,28 +1,20 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const { checkUser } = require("./middleWare/AuthMiddleware");
-const app = express();
 
 const Routes = require("./routes/Routes");
 
-const dbUri =
-  "mongodb+srv://dovydas372:dovydas372@node1.4u7tomw.mongodb.net/Projektas";
+const app = express();
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
-});
-
-mongoose.connect(dbUri);
-// .then(() => {
-//   app.listen(3000, "0.0.0.0", () => {
-//     console.log("Server running on port 3000");
-//   });
-// })
-// .catch((err) => console.error("DB connection error:", err));
+const dbUri = process.env.MONGO_URI;
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+
 app.use(express.static(path.join(__dirname, "assets")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -32,5 +24,16 @@ app.use(checkUser);
 app.use(Routes);
 
 app.use((req, res) => {
-  res.status(404).send("404", { title: "klaida" });
+  res.status(404).send("404 - Page not found");
 });
+
+mongoose
+  .connect(dbUri)
+  .then(() => {
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
